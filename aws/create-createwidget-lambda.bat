ec1c9e1194ca2942e0538c449320d3ff5c61335e
Robert Lewis <rablewis@algo-pop.com>
2022-01-20 19:33:38 +0000

    Add CreateWidget function

--- aws/create-createwidget-lambda.bat
+++ aws/create-createwidget-lambda.bat
@@ -2,12 +2,12 @@ REM TODO: Parameterize function name, handler name and zip file name.
 copy ..\build\distributions\aws-crud-0.1-SNAPSHOT.zip .
 
 aws lambda create-function^
- --function-name GetWidgets^
+ --function-name CreateWidget^
  --runtime java11^
  --role arn:aws:iam::184936849605:role/Lambda_WidgetCrud^
- --timeout 30
- --memory-size 256
- --handler com.algopop.awscrud.GetListHandler::handleRequest^
+ --timeout 30^
+ --memory-size 256^
+ --handler com.algopop.awscrud.CreateWidgetHandler::handleRequest^
  --zip-file fileb://aws-crud-0.1-SNAPSHOT.zip
 
 del aws-crud-0.1-SNAPSHOT.zip
