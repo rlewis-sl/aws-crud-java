6ef9252e48cd06f939667f6e333c15143a7a5984
Robert Lewis <rablewis@algo-pop.com>
2022-02-15 00:34:07 +0000

    Add mongodb option to UpdateWidgetHandler

--- src/main/java/com/algopop/awscrud/UpdateWidgetHandler.java
+++ src/main/java/com/algopop/awscrud/UpdateWidgetHandler.java
@@ -9,12 +9,27 @@ import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.result.InsertOneResult;
+import com.mongodb.client.result.UpdateResult;
+
+import org.bson.BsonValue;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.algopop.awscrud.MongoDb.WIDGETS_DEMO_DB;
+import static com.algopop.awscrud.MongoDb.WIDGET_COLLECTION;
+import static com.algopop.awscrud.mongodb.Widgets.widgetToDocument;
+
 
 public class UpdateWidgetHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+    private static final boolean IS_MONGODB = true;
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
 
@@ -36,20 +51,30 @@ public class UpdateWidgetHandler implements RequestHandler<APIGatewayV2HTTPEvent
         Widget widget = gson.fromJson(body, Widget.class);
 
         String id = request.getPathParameters().get("id");
-        Widget updatedWidget = updateWidget(id, widget);
+        try {
+            Widget updatedWidget = updateWidget(id, widget);
 
-        response.setStatusCode(200);
-    
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Content-Type", "application/json");
-        response.setHeaders(headers);
+            response.setStatusCode(200);
+        
+            Map<String, String> headers = new HashMap<>();
+            headers.put("Content-Type", "application/json");
+            response.setHeaders(headers);
 
-        response.setBody(gson.toJson(updatedWidget));
+            response.setBody(gson.toJson(updatedWidget));
+
+        } catch (Exception ex) {
+            response.setStatusCode(500);
+            response.setHeaders(new HashMap<>());
+        }
 
         return response;
     }
 
     private Widget updateWidget(String id, Widget widget) {
+        if (IS_MONGODB) {
+            return updateWidgetMongoDb(id, widget);
+        }
+
         String widgetId = widget.getId();
         if (widgetId.isBlank()) {
             widget.setId(id);
@@ -58,5 +83,29 @@ public class UpdateWidgetHandler implements RequestHandler<APIGatewayV2HTTPEvent
         }
         
         return Widgets.updateWidget(widget);
+    }
+
+    private Widget updateWidgetMongoDb(String id, Widget widget) {
+        String widgetId = widget.getId();
+        if (widgetId.isBlank()) {
+            widget.setId(id);
+        } else if (!widgetId.equals(id)) {
+            throw new IllegalArgumentException();  // should really return a 400 error of some kind
+        }
+
+        Document doc = widgetToDocument(widget);
+
+        final String connectionString = MongoDb.getConnectionString();
+        MongoClient mongoClient = MongoDb.getClient(connectionString);
+        try {
+            MongoCollection<Document> collection = MongoDb.getCollection(mongoClient, WIDGETS_DEMO_DB, WIDGET_COLLECTION);
+            Bson filter = Filters.eq("_id", new ObjectId(id));
+            collection.replaceOne(filter, doc);
+
+            return widget;
+
+        } finally {
+            mongoClient.close();
+        }
     }
 }
