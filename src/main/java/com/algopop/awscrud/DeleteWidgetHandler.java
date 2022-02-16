1ac7af1642ed276c43623e23db6ed2f7529c22ec
Robert Lewis <rablewis@algo-pop.com>
2022-02-16 15:50:02 +0000

    Implement delete for MongoDB

--- src/main/java/com/algopop/awscrud/DeleteWidgetHandler.java
+++ src/main/java/com/algopop/awscrud/DeleteWidgetHandler.java
@@ -5,9 +5,20 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayV2HTTPResponse;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+
+import org.bson.Document;
+import org.bson.conversions.Bson;
+import org.bson.types.ObjectId;
+
+import static com.algopop.awscrud.MongoDb.WIDGETS_DEMO_DB;
+import static com.algopop.awscrud.MongoDb.WIDGET_COLLECTION;
 
 
 public class DeleteWidgetHandler implements RequestHandler<APIGatewayV2HTTPEvent, APIGatewayV2HTTPResponse> {
+    private static final boolean IS_MONGODB = true;
 
     @Override
     public APIGatewayV2HTTPResponse handleRequest(APIGatewayV2HTTPEvent request, Context context) {
@@ -33,6 +44,22 @@ public class DeleteWidgetHandler implements RequestHandler<APIGatewayV2HTTPEvent
     }
 
     private void deleteWidget(String id) {
-        Widgets.deleteWidget(id);
+        if (IS_MONGODB) {
+            deleteWidgetMongoDb(id);
+        } else {
+            Widgets.deleteWidget(id);
+        }
+    }
+
+    private void deleteWidgetMongoDb(String id) {
+        final String connectionString = MongoDb.getConnectionString();
+        MongoClient mongoClient = MongoDb.getClient(connectionString);
+        try {
+            MongoCollection<Document> collection = MongoDb.getCollection(mongoClient, WIDGETS_DEMO_DB, WIDGET_COLLECTION);
+            Bson filter = Filters.eq("_id", new ObjectId(id));
+            collection.deleteOne(filter);
+        } finally {
+            mongoClient.close();
+        }
     }
 }
